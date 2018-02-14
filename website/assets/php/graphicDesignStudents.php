04524a6ef32884616cdbbbaa9bdaf98748c834f0
Erin Vance <erinavance@yahoo.com>
2018-02-14 12:30:01 -0600

    edited to work with image hover effect

--- website/assets/php/graphicDesignStudents.php
+++ website/assets/php/graphicDesignStudents.php
@@ -1,6 +1,6 @@
 <?php
 for ($i=40; $i < 60; $i++) {
-echo "<img class='picture' onclick='modalDisplay()' id ='GD $i' width='200px'  src='assets/img/girl.jpg'/>";
+echo "<span class='imageBorder'><img class='picture' onclick='modalDisplay()' id ='GD $i' width='200px'  src='assets/img/girl.jpg'/></span>";
 }
 
 ?>

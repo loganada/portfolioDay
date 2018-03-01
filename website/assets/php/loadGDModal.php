353148fad27c553e58901fad8dac56dbfbaf88ae
Adam logan <loganada5@gmail.com>
2018-03-01 16:06:43 -0600

    adding for phuc

---
+++ website/assets/php/loadGDModal.php
@@ -0,0 +1,31 @@
+<?php
+  if ("#GD 1") {
+    echo "<img class='picture' id ='modalGD 1' width='200px'
+            src='assets/img/dpd-photos/GD-1.jpg'/>";
+            if ("#GD 2") {
+              echo "<img class='picture' id ='modalGD 2' width='200px'
+                      src='assets/img/dpd-photos/GD-2.jpg'/>";
+            }
+  }
+
+  if ("#GD 3") {
+    echo "<img class='picture' id ='modalGD 3' width='200px'
+            src='assets/img/dpd-photos/GD-3.jpg'/>";
+  }
+  if ("#GD 4") {
+    echo "<img class='picture' id ='modalGD $i' width='200px'
+            src='assets/img/dpd-photos/GD-$i.jpg'/>";
+  }
+  if ("#GD 5") {
+    echo "<img class='picture' id ='modalGD $i' width='200px'
+            src='assets/img/dpd-photos/GD-$i.jpg'/>";
+  }
+  if ("#GD 6") {
+    echo "<img class='picture' id ='modalGD $i' width='200px'
+            src='assets/img/dpd-photos/GD-$i.jpg'/>";
+  }
+
+
+
+
+ ?>

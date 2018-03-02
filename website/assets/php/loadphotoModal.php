c79d3aaf2abf7aa9bd63c70ee345b9ae395bade0
Dan Schneider <Dschnd@gmail.com>
2018-03-01 19:58:07 -0600

    Responsiveness and add video production tabs

---
+++ website/assets/php/loadphotoModal.php
@@ -0,0 +1,53 @@
+<?php
+$q = $_REQUEST["q"];
+
+  if ($q == "photo 1") {
+    echo "<img class='picture' id ='modalphoto 1' width='200px'
+            src='assets/img/dpd-photos/photo-1.jpg'/>";
+  }
+  if ($q == "photo 2") {
+    echo "<img class='picture' id ='modalphoto 2' width='200px'
+            src='assets/img/dpd-photos/photo-2.jpg'/>";
+  }
+
+  if ($q == "photo 3") {
+    echo "<img class='picture' id ='modalphoto 3' width='200px'
+            src='assets/img/dpd-photos/photo-3.jpg'/>";
+  }
+  if ($q == "photo 4") {
+    echo "<img class='picture' id ='modalphoto 4' width='200px'
+            src='assets/img/dpd-photos/photo-4.jpg'/>";
+  }
+  if ($q == "photo 5") {
+    echo "<img class='picture' id ='modalphoto 5' width='200px'
+            src='assets/img/dpd-photos/photo-5.jpg'/>";
+  }
+  if ($q == "photo 6") {
+    echo "<img class='picture' id ='modalphoto 6' width='200px'
+            src='assets/img/dpd-photos/photo-6.jpg'/>";
+  }
+  if ($q == "photo 7") {
+    echo "<img class='picture' id ='modalphoto 7' width='200px'
+            src='assets/img/dpd-photos/photo-7.jpg'/>";
+  }
+  if ($q == "photo 8") {
+    echo "<img class='picture' id ='modalphoto 8' width='200px'
+            src='assets/img/dpd-photos/photo-8.jpg'/>";
+  }
+  if ($q == "photo 9") {
+    echo "<img class='picture' id ='modalphoto 9' width='200px'
+            src='assets/img/dpd-photos/photo-9.jpg'/>";
+  }
+  if ($q == "photo 10") {
+    echo "<img class='picture' id ='modalphoto 10' width='200px'
+            src='assets/img/dpd-photos/photo-10.jpg'/>";
+  }
+  if ($q == "photo 11") {
+    echo "<img class='picture' id ='modalphoto 11' width='200px'
+            src='assets/img/dpd-photos/photo-11.jpg'/>";
+  }
+
+
+
+
+ ?>

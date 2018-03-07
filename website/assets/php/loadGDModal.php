a5ffc4a8df61adfc76e2cd0869f260f61e8bdbab
webdevAndres <macias.andres02@gmail.com>
2018-03-07 09:23:12 -0600

    modal shows up onclick, exits with span (x)

--- website/assets/php/loadGDModal.php
+++ website/assets/php/loadGDModal.php
@@ -28,7 +28,7 @@ $GD = array();
   "<h4 class='modalText descrip' >Three Words That Describe Me</h4>" .
  "<h3 class='modalText center'>" . $arrayGD[$i]['bio_three_words'] . "</h3>" .
  "<img class='picture' id ='modalPhoto' src='assets/img/dpd-photos/GD-$i.jpg'/><br>" .
- "<span class='close'>X</span>";
+ "<span class='close' onclick='closeModal()'>X</span>";
 
 }
 if ($q == "GD 0") {

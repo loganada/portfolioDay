4366937f2efc1f86aed78570e1fe3c0ba9027160
webdevAndres <macias.andres02@gmail.com>
2018-03-27 15:35:11 -0500

    photography student section updated

--- website/assets/php/photographyStudents.php
+++ website/assets/php/photographyStudents.php
@@ -1,7 +1,7 @@
 <?php
 include "loadData.php";
 
-for ($i=0; $i < 11; $i++) {
+for ($i=0; $i < 14; $i++) {
 echo "<span class='imageBorder student-overlay'>
 		<img class='picture'   onclick='modalDisplay()' id ='photo $i' width='200px'  src='assets/img/dpd-photos/photo-$i.jpg'/>
 </span>";

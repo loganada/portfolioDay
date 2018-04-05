1a6fae3e7419e44b4570bf1238397118864e7af1
Adam logan <loganada5@gmail.com>
2018-04-05 12:51:31 -0500

    fixing duplicates

--- website/assets/php/photographyStudents.php
+++ website/assets/php/photographyStudents.php
@@ -2,12 +2,18 @@
 include "loadData.php";
 
 for ($i=0; $i < 14; $i++) {
+	if ($i==7 || $i==11){
+
+	}
+	else{
+
+
 	echo "<span class='imageBorder student-overlay'>
 			<img class='picture' onclick='modalDisplay()'  id ='".$arrayphoto[$i]['bio_last_name'] . ".". $arrayphoto[$i]['bio_first_name'] ."'' width='200px'  src='assets/img/finalPicsPhoto/" . $arrayphoto[$i]['bio_last_name'] . ".". $arrayphoto[$i]['bio_first_name'] .
 		".jpg'/>
 			<span class='student-overlay-info student-overlay-bg'><h3>" . $arrayphoto[$i]['bio_first_name'] . " ". $arrayphoto[$i]['bio_last_name'] .
 		"</h3></span></span>";
-
+}
 }
 for ($i=13; $i <14; $i++) {
 echo "<span class='imageBorder student-overlay'>
